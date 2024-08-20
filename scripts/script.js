cffed1c237bd1f78b4d1f96e5b9c7db2cc4f43f9
Ryan de Souza Silva <107144128+RyanSS27@users.noreply.github.com>
2024-08-20 13:43:26 -0300

    implementação dos nosso serviços

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,16 @@
 var infData = new Date()
 var hora = infData.getHours()
 var teste = window.document.querySelector('h1#titulo')
-window.document.body.addEventListener('load', carregar)
+window.document.body.addEventListener('load', carregar())
 
-var fundo = document.body.style
+
+
+
 
 function carregar() {
-    if (hora > 6 && hora < 18) {
-        fundo.backgroundImage = 'url(../pictures/dia.png)'
+    var fundo = document.body
+    
+    if (hora > 5 && hora < 18) {
+        //mudar o css do fundo e das cores do form, .entrar, .legenda e h1
     }
 }
